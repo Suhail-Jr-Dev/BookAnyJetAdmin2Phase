392ebefde2528b6b687a6c964491535eef19f0b8
Suhail-sJrD <suhailsince2003@gmail.com>
2024-10-28 12:25:15 +0530

    staring the login second phase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 import RegisterForm from '../components/Categories/RegisterForm';
 import { Spin } from 'antd'
 
+import loginImg from '../assets/images/loginImg.jpeg'
+
 const Login = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
@@ -77,80 +79,49 @@ const Login = () => {
 
 
   return (
-    <div className="flex items-center relative justify-center min-h-screen">
+    <div className="flex items-center relative gap-7 flex-wrap justify-center min-h-screen">
       {
         loading && <div className="z-40 bg-gray-300 inset-0 bg-opacity-40 flex items-center absolute justify-center">
           <Spin size="large" />
         </div>
       }
-      <div className="w-full max-w-lg p-12 space-y-8 transition duration-500 transform rounded-lg shadow-2xl hover:scale-105">
-        <h1 className="text-3xl font-bold text-center text-blue-800">Welcome Back!</h1>
-        <form class="max-w-sm mx-auto w-[100%] p-5 rounded-lg" onSubmit={onFinish} >
 
-          <div class="mb-5">
-            <label for="email" class="block mb-2 text-sm font-medium  dark:text-black"> Email</label>
-            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Email" required onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div class="mb-5">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">password</label>
-            <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required onChange={(e) => setPassword(e.target.value)} />
-          </div>
+      <div className='bg-gray-300 flex-wrap flex w-[55rem] rounded-lg items-center justify-center'>
+        <img src={loginImg} alt="" className='w-[26rem]  rounded-lg  h-[33rem]' />
 
-          <div class="max-w-sm mx-auto">
-            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Select Role</label>
-            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setRole(e.target.value)}>
+        <div className="w-full lg:w-[52%] max-w-lg p-12 space-y-8">
+          <h1 className="text-3xl font-bold text-center text-LoginColor">Holla , Welcome Back!</h1>
+          <form class="max-w-sm mx-auto flex gap-1 flex-col w-[100%] p-5 rounded-lg" onSubmit={onFinish} >
 
-              <option value={''}>Select User Role</option>
-              <option value={'broker'}>broker</option>
-              <option value={'operator'}>operator</option>
-              <option value={'user-admin'}>user-admin</option>
-              <option value={'super-admin'}>super-admin</option>
-            </select>
-          </div>
-
-
-
-
-          <button type="submit" class="text-white my-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg border-none outline-none text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700">Log In </button>
-        </form>
-
-
-
-
-        {/* <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: 'Please enter your email!' }]}
-          >
-            <input
-              type="email"
-              className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please enter your password!' }]}
-          >
-            <input
-              type="password"
-              className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </Form.Item>
-          <div className="flex flex-col gap-4">
-            <button
-              className="w-full py-3 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              type="submit"
-            >
-              Login
-            </button>
-            <div className='flex gap-2'>
-              <h3>Dont have an account ?</h3>
-              <Link className='font-semibold text-blue-600 hover:underline' to={"/register"}>Register here</Link>
+            <div class="mb-5">
+              <label for="email" class="block mb-2 text-sm font-medium  dark:text-black"> Email</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 outline-none border border-gray-300 text-LoginColor text-sm rounded-lg block w-full p-2.5" placeholder="Email" required onChange={(e) => setEmail(e.target.value)} />
             </div>
-          </div>
-        </Form> */}
+
+            <div class="mb-5 relative">
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+
+              <a href="#" className='underline text-LoginColor absolute bottom-0 right-0 top-[4.5rem]'> Forgot Password?</a>
+
+              <input type="password" id="password"
+                placeholder="Password" class="shadow-sm bg-gray-50 border outline-none border-gray-300 text-LoginColor text-sm rounded-lg block w-full p-2.5" required onChange={(e) => setPassword(e.target.value)} />
+            </div>
+
+            <div class="max-w-sm mx-auto w-full">
+              <label for="countries" class="block  mb-2 text-sm font-medium text-gray-900">Select Role</label>
+              <select id="countries" class="bg-gray-50 border border-gray-300 outline-none text-LoginColor text-sm rounded-lg block w-full p-2.5 " onChange={(e) => setRole(e.target.value)}>
+
+                <option value={''}>Select User Role</option>
+                <option value={'broker'}>Broker</option>
+                <option value={'operator'}>Operator</option>
+                <option value={'user-admin'}>User-admin</option>
+                <option value={'super-admin'}>Super-admin</option>
+              </select>
+            </div>
+
+            <button type="submit" class="text-white my-4 font-medium rounded-lg border-none outline-none text-sm px-5 py-2.5 text-center bg-LoginColor w-full">Log In </button>
+          </form>
+        </div>
       </div>
 
       {/* <RegisterForm/> */}
